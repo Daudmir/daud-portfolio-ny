8759ca9297855adf011b6e80bf216e96ba44a8c6
Daudmir <daud0013@edu.easj.dk>
2021-10-13 01:03:10 +0200

    Update social-icon-list.js

--- src/components/_config/social-icon-list.js
+++ src/components/_config/social-icon-list.js
@@ -1,18 +1,13 @@
 export const socialIconList = [
   {
-    name: 'twitter',
+    name: 'linkedin',
     prefix: 'fab', // fab prefix for brands, fas for solid icons
-    link: 'https://twitter.com/',
+    link: 'https://www.linkedin.com/in/daud-mir-314bb6220/',
   },
   {
     name: 'github',
     prefix: 'fab',
-    link: 'https://github.com/knochenmark/gatsby-starter-level-2',
-  },
-  {
-    name: 'codepen',
-    prefix: 'fab',
-    link: 'https://codepen.io/',
+    link: 'https://github.com/Daudmir',
   },
   // Add further social links with the icon of choice and link here
   // check https://fontawesome.com/icons?d=gallery&s=brands for other brand icons
