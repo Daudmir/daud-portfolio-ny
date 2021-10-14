d5959295895b36b2a0e068de68900d98f909aec0
Daudmir <daud0013@edu.easj.dk>
2021-10-14 03:17:59 +0200

    Update styled-headings.js

--- src/components/_shared/styled-headings.js
+++ src/components/_shared/styled-headings.js
@@ -5,34 +5,11 @@ export const StyledH1 = styled.h1`
   position: relative;
   font-weight: 800;
   margin: 20px 0;
-
-  &:first-letter {
-    color: var(--primary-color);
-  }
-
-  &:after {
-    background-color: var(--primary-color);
-    content: '';
-    position: absolute;
-    top: -30px;
-    left: 0;
-    width: 30px;
-    height: 2px;
-  }
+  
 `;
 
 export const StyledH2 = styled.h2`
   color: var(--title-color);
   margin: 20px 0;
   position: relative;
-
-  &:after {
-    background-color: var(--primary-color);
-    content: '';
-    position: absolute;
-    top: -20px;
-    left: 0;
-    width: 30px;
-    height: 2px;
-  }
 `;
